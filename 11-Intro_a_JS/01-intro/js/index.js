13d1b65947a5c29215d18e3fc59b0590555b671d
Paco Menchaca <pacocoder86@outlook.com>
2022-01-24 21:30:31 -0600

    intro a JS

---
+++ 11-Intro_a_JS/01-intro/js/index.js
@@ -0,0 +1,14 @@
+// muestra la salida en la consola
+console.log("Hola mundo");
+console.log(5 + 10);
+console.log(4 === 3);
+
+//muestra la salida en  la página
+document.write("Hola mundo cruel");
+document.write(5 + 10);
+document.write(4 === "4");
+
+//Mustra un popup con la salida de datos
+alert("Hola mundo cruel");
+alert(5 + 10);
+alert(4 === "4");
